9adb9d9f14c5b42d5150cabf048d689654e2b51e
PrasannaThuraisingam647 <55007017+PrasannaThuraisingam647@users.noreply.github.com>
2021-09-16 12:34:31 -0400

    V1: Commit

---
+++ src/components/Counter.vue
@@ -0,0 +1,30 @@
+<template>
+
+  <h1>0:0:{{counter}}</h1>
+  <button @click="start" class="ring-4 ring-indigo-300">
+    Increment Counter
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'Counter',
+  props: {
+  },
+  data() {
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    start() {
+      this.counter = this.counter + 1;
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
